a7d2bb19c06f00adf0de551ed18868895353f9e2
tuxa <69870860+mrtuxa@users.noreply.github.com>
2022-12-07 15:20:47 +0000

    Create main.go

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"github.com/fatih/color"
+	"github.com/mrtuxa/gosh/system"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		system.ShellLine()
+		input, _ := reader.ReadString('\n')
+		input = strings.Replace(input, "\n", "", -1)
+
+		if system.Command("btw", input) == 0 {
+			system.Println("I Use arch btw")
+		} else if system.Command("exit", input) == 0 {
+			color.Green("Goodbye")
+			system.Exit()
+		} else if system.Command("clear", input) == 0 {
+			system.ClearScreen()
+		} else {
+			_, err := system.ExecCommand(input)
+			if err != nil {
+				return
+			}
+
+		}
+	}
+}
